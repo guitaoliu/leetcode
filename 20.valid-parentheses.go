a2621a7bd7c67569d1151ca290a6f9844348573e
Guitao Liu <guitao.liu@Outlook.com>
2020-09-22 23:28:09 +0800

    finish some tasks

---
+++ 20.valid-parentheses.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=20 lang=golang
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+func isValid(s string) bool {
+	if len(s) <= 1 {
+		return false
+	}
+	stack := make([]rune, 0)
+	for _, v := range s {
+		if (v == '[') || (v == '(') || (v == '{') {
+			stack = append(stack, v)
+		} else if ((v == ']') && len(stack) > 0 && stack[len(stack)-1] == '[') ||
+			((v == ')') && len(stack) > 0 && stack[len(stack)-1] == '(') ||
+			((v == '}') && len(stack) > 0 && stack[len(stack)-1] == '{') {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
+
+// @lc code=end
+
