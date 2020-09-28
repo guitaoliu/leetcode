8d7fe4bea955c5d9ee570e07e1ba49996c31f0f5
Guitao Liu <guitao.liu@Outlook.com>
2020-09-28 12:28:34 +0800

    finish 28

---
+++ 28.implement-str-str.go
@@ -0,0 +1,30 @@
+package Leetcode
+
+/*
+ * @lc app=leetcode id=28 lang=golang
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+	for i := 0; ; i++ {
+		for j := 0; ; j++ {
+			if j == len(needle) {
+				return i
+			}
+			if i+j == len(haystack) {
+				return -1
+			}
+			if needle[j] != haystack[i+j] {
+				break
+			}
+		}
+	}
+}
+
+// func strStr(haystack string, needle string) int {
+// 	return strings.Index(haystack, needle)
+// }
+
+// @lc code=end
