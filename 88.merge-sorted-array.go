7cf46fc50116cf7e78640da7191831335d98b6a5
Guitao Liu <guitao.liu@Outlook.com>
2020-10-14 16:11:47 +0800

    add solution: 88 merge sorted array

---
+++ 88.merge-sorted-array.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=88 lang=golang
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	if m == 0 {
+		copy(nums1, nums2)
+		return
+	}
+	i, j, k := m-1, n-1, m+n-1
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+	for j >= 0 {
+		nums1[k] = nums2[j]
+		j--
+		k--
+	}
+}
+
+// @lc code=end
+
