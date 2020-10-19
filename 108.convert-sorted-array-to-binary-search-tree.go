c411639a5260ab27508d12c0b71e3807313970cd
Guitao Liu <guitao.liu@Outlook.com>
2020-10-19 15:50:38 +0800

    add solution: 108 Convert Sorted Array to Binary Search Tree

---
+++ 108.convert-sorted-array-to-binary-search-tree.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=108 lang=golang
+ *
+ * [108] Convert Sorted Array to Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	return &TreeNode{Val: nums[len(nums)/2], Left: sortedArrayToBST(nums[:len(nums)/2]), Right: sortedArrayToBST(nums[len(nums)/2+1:])}
+}
+
+// @lc code=end
+
