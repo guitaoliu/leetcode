d3558fd7bfb56b38c5521cc2d19dc9af4235d993
Guitao Liu <guitao.liu@Outlook.com>
2020-12-07 12:49:16 +0800

    add solution 538 convert BST to greater tree

---
+++ 538.convert-bst-to-greater-tree.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=538 lang=golang
+ *
+ * [538] Convert BST to Greater Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func convertBST(root *TreeNode) *TreeNode {
+	cnt := 0
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Right)
+		cnt += node.Val
+		node.Val = cnt
+		dfs(node.Left)
+	}
+	dfs(root)
+	return root
+}
+
+// @lc code=end
+
