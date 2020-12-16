11b5bd0b5627c1b281807623ff4c1bb4149c3e2d
Guitao Liu <guitao.liu@Outlook.com>
2020-12-16 11:12:29 +0800

    add solution 589 n ary tree preorder traversal

---
+++ 589.n-ary-tree-preorder-traversal.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=589 lang=golang
+ *
+ * [589] N-ary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func preorder(root *Node) []int {
+	if root == nil {
+		return nil
+	}
+	res := []int{}
+	stack := []*Node{root}
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, node.Val)
+		reversedChildren := reverse(node.Children)
+		stack = append(stack, reversedChildren...)
+	}
+	return res
+}
+
+func reverse(list []*Node) []*Node {
+	for i, j := 0, len(list)-1; i < j; i, j = i+1, j-1 {
+		list[i], list[j] = list[j], list[i]
+	}
+	return list
+}
+
+// @lc code=end
+
