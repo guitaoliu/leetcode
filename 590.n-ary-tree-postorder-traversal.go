a8b2c1ac04ca0ae2e7788241896a10d3deaa7b8d
Guitao Liu <guitao.liu@Outlook.com>
2020-12-17 16:35:14 +0800

    add solution 590 n ary tree postorder traversal

---
+++ 590.n-ary-tree-postorder-traversal.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=590 lang=golang
+ *
+ * [590] N-ary Tree Postorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func postorder(root *Node) []int {
+	if root == nil {
+		return nil
+	}
+	stack := []*Node{root}
+	res := []int{}
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, node.Val)
+		stack = append(stack, node.Children...)
+	}
+	return res
+}
+
+// @lc code=end
+
