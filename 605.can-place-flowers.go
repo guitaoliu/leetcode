89227f9ded924e9a78fac0f2ea7d3dff8cb27092
Guitao Liu <guitao.liu@Outlook.com>
2020-12-22 08:48:34 +0800

    add solution 605 can place flowers

---
+++ 605.can-place-flowers.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=605 lang=golang
+ *
+ * [605] Can Place Flowers
+ */
+
+// @lc code=start
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	for i := 0; i < len(flowerbed); i++ {
+		if flowerbed[i] == 0 &&
+			(i == 0 || flowerbed[i-1] == 0) &&
+			(i == len(flowerbed)-1 || flowerbed[i+1] == 0) {
+			n--
+			flowerbed[i] = 1
+		}
+	}
+	return n <= 0
+}
+
+// @lc code=end
+
