8c8af46a805c99afb6d29f340139bd332d1eb10e
Guitao Liu <guitao.liu@Outlook.com>
2021-02-09 09:36:21 +0800

    reduce memory usage

--- 64.minimum-path-sum.go
+++ 64.minimum-path-sum.go
@@ -7,24 +7,19 @@
 // @lc code=start
 func minPathSum(grid [][]int) int {
 	m, n := len(grid), len(grid[0])
-	res := make([][]int, m)
-	for i := 0; i < m; i++ {
-		res[i] = make([]int, n)
-	}
-	res[0][0] = grid[0][0]
 	for i := 1; i < m; i++ {
-		res[i][0] = grid[i][0] + res[i-1][0]
+		grid[i][0] += grid[i-1][0]
 	}
 	for i := 1; i < n; i++ {
-		res[0][i] = grid[0][i] + res[0][i-1]
+		grid[0][i] += grid[0][i-1]
 	}
 
 	for i := 1; i < m; i++ {
 		for j := 1; j < n; j++ {
-			res[i][j] = min(res[i-1][j], res[i][j-1]) + grid[i][j]
+			grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j]
 		}
 	}
-	return res[m-1][n-1]
+	return grid[m-1][n-1]
 }
 
 func min(a, b int) int {
