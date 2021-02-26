58b88634ff608d3e7267e137dc6f89b5fff00d73
Guitao Liu <guitao.liu@Outlook.com>
2021-02-26 10:18:36 +0800

    add solution 94

---
+++ 94.binary-tree-inorder-traversal.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=94 lang=golang
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	stack := []*TreeNode{}
+	res := make([]int, 0)
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+	return res
+}
+
+// @lc code=end
+
