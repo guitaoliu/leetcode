7d4bf6ed922219adf39631b877b9ec9daffb9582
Guitao Liu <guitao.liu@Outlook.com>
2021-03-01 10:02:42 +0800

    add solution 102

---
+++ 102.binary-tree-level-order-traversal.go
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=102 lang=golang
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	queue := []*TreeNode{root}
+	res := make([][]int, 0)
+	for len(queue) > 0 {
+		lenQueue := len(queue)
+		tmp := make([]int, 0)
+		for i := 0; i < lenQueue; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			tmp = append(tmp, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		res = append(res, tmp)
+	}
+	return res
+}
+
+// @lc code=end
+
