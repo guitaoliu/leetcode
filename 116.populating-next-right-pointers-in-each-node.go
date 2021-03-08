d7b9f5b19150e7a58d5d86f1595ec07958fe62af
Guitao Liu <guitao.liu@Outlook.com>
2021-03-08 09:25:01 +0800

    add solution 116 & 117

---
+++ 116.populating-next-right-pointers-in-each-node.go
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=116 lang=golang
+ *
+ * [116] Populating Next Right Pointers in Each Node
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	stack := []*Node{root}
+	for len(stack) > 0 {
+		cur := stack
+		stack = nil
+		for i := 0; i < len(cur); i++ {
+			if i < len(cur)-1 {
+				cur[i].Next = cur[i+1]
+			}
+			if cur[i].Left != nil {
+				stack = append(stack, cur[i].Left)
+			}
+			if cur[i].Right != nil {
+				stack = append(stack, cur[i].Right)
+			}
+		}
+	}
+	return root
+}
+
+// @lc code=end
+
