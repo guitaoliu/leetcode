bc904963395f196aa1ace0f9a44bb0450e99657a
Guitao Liu <guitao.liu@Outlook.com>
2021-03-19 08:45:41 +0800

    add solution 144

---
+++ 144.binary-tree-preorder-traversal.go
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=144 lang=golang
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	res := make([]int, 0)
+	stack := []*TreeNode{root}
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, node.Val)
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+		}
+	}
+	return res
+}
+
+// @lc code=end
+
