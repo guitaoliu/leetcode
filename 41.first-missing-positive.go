586fa54e509b57a09a348cd53de87b3464264b85
Guitao Liu <guitao.liu@Outlook.com>
2021-03-29 19:45:29 +0800

    add solution 41

---
+++ 41.first-missing-positive.go
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=41 lang=golang
+ *
+ * [41] First Missing Positive
+ */
+
+// @lc code=start
+func firstMissingPositive(nums []int) int {
+	for i := 0; i < len(nums); i++ {
+		for nums[i] > 0 && nums[i] <= len(nums) && nums[nums[i]-1] != nums[i] {
+			nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+		}
+	}
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+	return len(nums) + 1
+}
+
+// @lc code=end
+
