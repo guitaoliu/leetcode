e6fc2c5e8e992e409b463314b943db9ff05e02a1
Guitao Liu <guitao.liu@Outlook.com>
2021-04-05 19:17:42 +0800

    add solution 200

---
+++ 200.number-of-islands.go
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=200 lang=golang
+ *
+ * [200] Number of Islands
+ */
+
+// @lc code=start
+func numIslands(grid [][]byte) int {
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return 0
+	}
+	res := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == '1' {
+				res++
+				dfs(grid, i, j)
+			}
+		}
+	}
+	return res
+}
+
+func dfs(grid [][]byte, i, j int) {
+	if i < 0 || i > len(grid)-1 || j < 0 || j > len(grid[0])-1 ||
+		grid[i][j] == '0' {
+		return
+	}
+	grid[i][j] = '0'
+	dfs(grid, i+1, j)
+	dfs(grid, i-1, j)
+	dfs(grid, i, j+1)
+	dfs(grid, i, j-1)
+}
+
+// @lc code=end
+
