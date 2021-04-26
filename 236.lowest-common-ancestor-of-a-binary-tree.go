ad4fdd048819f81b54f1dccade67661c1c0244ff
Guitao Liu <guitao.liu@Outlook.com>
2021-04-26 09:21:10 +0800

    add solution 236

--- 236.lowest-common-ancestor-of-a-binary-tree.go
+++ 236.lowest-common-ancestor-of-a-binary-tree.go
@@ -13,8 +13,24 @@
  *     Right *TreeNode
  * }
  */
- func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-  
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil {
+		return right
+	}
+	return left
 }
+
 // @lc code=end
 
