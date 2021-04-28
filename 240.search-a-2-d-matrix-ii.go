2182b2293d93738d98ca4e78bccfac173d3e41a0
Guitao Liu <guitao.liu@Outlook.com>
2021-04-28 14:49:42 +0800

    add solution 240

---
+++ 240.search-a-2-d-matrix-ii.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=240 lang=golang
+ *
+ * [240] Search a 2D Matrix II
+ */
+
+// @lc code=start
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	row, col := 0, len(matrix[0])-1
+	for col >= 0 && row <= len(matrix)-1 {
+		if target == matrix[row][col] {
+			return true
+		} else if target > matrix[row][col] {
+			row++
+		} else {
+			col--
+		}
+	}
+	return false
+}
+
+// @lc code=end
+
