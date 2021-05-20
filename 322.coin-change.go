be5ee187242837733bd66ca6d87a489044c8b0c7
Guitao Liu <guitao.liu@Outlook.com>
2021-05-20 09:16:30 +0800

    add solution 322

---
+++ 322.coin-change.go
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=322 lang=golang
+ *
+ * [322] Coin Change
+ */
+
+// @lc code=start
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 0
+	for i := 1; i < len(dp); i++ {
+		dp[i] = amount + 1
+	}
+	for i := 1; i < len(dp); i++ {
+		for j := 0; j < len(coins); j++ {
+			if coins[j] <= i {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
+			}
+		}
+	}
+	if dp[amount] > amount {
+		return -1
+	}
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
+
