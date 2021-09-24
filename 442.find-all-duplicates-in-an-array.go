5a1821fcb4fd1bb6b5ded9a9c8746a067de6da40
guitaoliu <guitao.liu@outlook.com>
2021-09-24 10:06:28 -0400

    add solution 422

---
+++ 442.find-all-duplicates-in-an-array.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=442 lang=golang
+ *
+ * [442] Find All Duplicates in an Array
+ */
+
+// @lc code=start
+func findDuplicates(nums []int) []int {
+	res := []int{}
+	for i := 0; i < len(nums); i++ {
+		cur := abs(nums[i])
+		if nums[cur-1] < 0 {
+			res = append(res, -nums[cur-1])
+		} else {
+			nums[cur-1] *= -1
+		}
+
+	}
+	return res
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+// @lc code=end
+
