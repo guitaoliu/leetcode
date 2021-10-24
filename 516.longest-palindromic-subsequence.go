2189c77c6468cc00998a9fa6d407a189ca4e10ca
Guitao Liu <gtliu52@gmail.com>
2021-10-24 10:14:28 -0400

    add solution 516

---
+++ 516.longest-palindromic-subsequence.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=516 lang=golang
+ *
+ * [516] Longest Palindromic Subsequence
+ */
+
+// @lc code=start
+func longestPalindromeSubseq(s string) int {
+	dp := make([][]int, len(s))
+	for i := 0; i < len(s); i++ {
+		dp[i] = make([]int, len(s))
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		dp[i][i] = 1
+		for j := i + 1; j < len(s); j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][len(s)-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
+
