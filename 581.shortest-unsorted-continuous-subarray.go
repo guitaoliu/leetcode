5a6c9a6ef1b96e7385c6488382ab1a6ed35204b7
guitaoliu <guitao.liu@outlook.com>
2021-11-10 20:53:30 -0500

    add solution 581

---
+++ 581.shortest-unsorted-continuous-subarray.go
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=581 lang=golang
+ *
+ * [581] Shortest Unsorted Continuous Subarray
+ */
+
+// @lc code=start
+func findUnsortedSubarrayTwoPointers(nums []int) int {
+	maxNum := math.MinInt64
+	minNum := math.MaxInt64
+	n := len(nums)
+	l, r := 0, n-1
+	for i := 0; i < n; i++ {
+		if nums[i] < maxNum {
+			r = i
+		}
+		if nums[i] > maxNum {
+			maxNum = nums[i]
+		}
+		if nums[n-i-1] > minNum {
+			l = n - i - 1
+		}
+		if nums[n-i-1] < minNum {
+			minNum = nums[n-i-1]
+		}
+	}
+	return r - l + 1
+}
+
+func findUnsortedSubarray(nums []int) int {
+	tmp := make([]int, len(nums))
+	copy(tmp, nums)
+	sort.Ints(tmp)
+	i, j := 0, len(nums)-1
+	for i <= j && nums[i] == tmp[i] {
+		i++
+	}
+	for i <= j && nums[j] == tmp[j] {
+		j--
+	}
+	return j - i + 1
+}
+
+// @lc code=end
+
