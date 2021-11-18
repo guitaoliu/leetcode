e76fe7bc74dd82c50da692ce5aae0a638a80c83a
guitaoliu <guitao.liu@outlook.com>
2021-11-18 16:36:11 -0500

    add solution 50

--- 35.search-insert-position.go
+++ 35.search-insert-position.go
@@ -5,7 +5,26 @@
  */
 
 // @lc code=start
+
 func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			if mid == len(nums)-1 || nums[mid+1] > target {
+				return mid + 1
+			}
+			left = mid + 1
+		}
+	}
+	return 0
+}
+
+func searchInsertON(nums []int, target int) int {
 	for i := 0; i < len(nums); i++ {
 		if nums[i] >= target {
 			return i
---
+++ 50.pow-x-n.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=50 lang=golang
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+	if n < 0 {
+		n = -n
+		x = 1 / x
+	}
+	tmp := myPow(x, n/2)
+	if n%2 == 0 {
+		return tmp * tmp
+	}
+	return tmp * tmp * x
+}
+
+// @lc code=end
+
