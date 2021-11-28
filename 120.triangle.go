3c75ea0cdabcbbffbbfd0e85a85e20dcd4b9c49b
guitaoliu <guitao.liu@outlook.com>
2021-11-28 12:46:03 -0500

    update 120

--- 120.triangle.go
+++ 120.triangle.go
@@ -6,6 +6,15 @@
 
 // @lc code=start
 func minimumTotal(triangle [][]int) int {
+	for i := len(triangle) - 2; i >= 0; i-- {
+		for j := 0; j < len(triangle[i]); j++ {
+			triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+		}
+	}
+	return triangle[0][0]
+}
+
+func minimumTotal(triangle [][]int) int {
 	if len(triangle) == 1 {
 		return triangle[0][0]
 	}
