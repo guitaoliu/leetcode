9e06cd2e42d570c2ff5135f82db9572f435abfbc
guitaoliu <gtliu52@gmail.com>
2021-12-25 10:08:14 -0500

    update solution 268

--- 268.missing-number.go
+++ 268.missing-number.go
@@ -6,6 +6,24 @@
 
 // @lc code=start
 func missingNumber(nums []int) int {
+    i := 0
+    for i < len(nums) {
+        if nums[i] < len(nums) && nums[i] != i {
+            j := nums[i]
+            nums[i], nums[j] = nums[j], nums[i]
+        } else {
+            i++
+        }
+    }
+    for i, n := range nums {
+        if i != n {
+            return i
+        }
+    }
+    return len(nums)
+}
+
+func missingNumber(nums []int) int {
 	res := 0
 	for i := 0; i < len(nums); i++ {
 		res = res + i + 1 - nums[i]
